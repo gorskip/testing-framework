cccbcfce6ac755c4024cfe809ffb54d0dad0601d
gorskip <prz.gorski@gmail.com>
2018-06-24 15:02:34 +0200

    changed type of diff value

--- src/main/java/verify/diff/Diff.java
+++ src/main/java/verify/diff/Diff.java
@@ -9,5 +9,5 @@ public class Diff {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String op;
     private String path;
-    private String value;
+    private Object value;
 }
